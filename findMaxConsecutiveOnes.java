2d0da7ba68ce64b86efabe613ef23486a310c536
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-02-09 19:48:40 +0530

    Add files via upload

---
+++ findMaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0,max=0;
+        for(int i:nums){
+            if(i==1){
+                count++;
+                if(count>max){
+                    max=count;
+                }
+            }
+            else count=0;
+        }
+        return max;
+    }
+}
